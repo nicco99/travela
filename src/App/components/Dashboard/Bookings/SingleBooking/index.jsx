9a73215ff0e9af758d7d32ab16e19cd1a2ca371c
Nicholas Njeru <nicholasnjeru917@gmail.com>
2022-12-04 20:23:31 +0300

    TRAV-21 added routing to the project

--- src/App/components/Dashboard/Bookings/SingleBooking/index.jsx
+++ src/App/components/Dashboard/Bookings/SingleBooking/index.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 function SingleBooking() {
+  const {id} = useParams()
+  console.log(id)
   return (
     <div>SingleBooking</div>
   )
