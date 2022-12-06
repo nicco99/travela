363ebb4c23f6803be5ea256cb8e7813f67923f1c
Nicholas Njeru <nicholasnjeru917@gmail.com>
2022-12-06 11:59:52 +0300

    TRAV-38  completed frontend updates

--- src/App/components/Dashboard/SideBar/index.jsx
+++ src/App/components/Dashboard/SideBar/index.jsx
@@ -15,7 +15,7 @@ function SideBar() {
             </Link>
 
             <Link
-              to=""
+              to="/dashboard"
               className="flex items-center p-2 text-base font-2xl  rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 text-white hover:text-black">
               <svg
                 aria-hidden="true"
@@ -31,7 +31,7 @@ function SideBar() {
           </li>
           <li>
             <Link
-              to="view_bookings"
+              to="bookings"
               className="flex items-center p-2 text-base font-2xl rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700  text-white hover:text-black">
               <svg
                 aria-hidden="true"
@@ -47,7 +47,7 @@ function SideBar() {
           </li>
           <li>
             <Link
-              to="book_a_trip"
+              to="book"
               className="flex items-center p-2 text-base font-2xl rounded-lg dark:text-white hover:bg-gray-100  text-white hover:text-black">
               <svg
                 aria-hidden="true"
@@ -66,7 +66,7 @@ function SideBar() {
           </li>
           <li>
             <Link
-              to="user_profile"
+              to="profile"
               className="flex items-center p-2 text-base font-2xl rounded-lg dark:text-white hover:bg-gray-100 text-white hover:text-black">
               <svg
                 aria-hidden="true"
@@ -105,7 +105,7 @@ function SideBar() {
           <li>
             <Link
               to=""
-              className="flex items-center p-2 text-base font-2xl text-white hover:text-blacktext-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+              className="flex items-center p-2 text-base font-2xl  text-white hover:text-blacktext-gray-900 rounded-lg dark:text-white hover:bg-gray-100 hover:text-black dark:hover:bg-gray-700">
               <svg
                 aria-hidden="true"
                 class="flex-shrink-0 w-5 h-5 text-white transition duration-75 group-hover:text-gray-900 dark:group-hover:text-white dark:text-gray-400"
@@ -126,7 +126,7 @@ function SideBar() {
           <li>
             <Link
               to=""
-              className="flex items-center p-2 text-base font-2xl text-white hover:text-blacktext-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+              className="flex items-center p-2 text-base font-2xl hover:text-black text-white hover:text-blacktext-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
               <svg
                 aria-hidden="true"
                 className="flex-shrink-0 w-6 h-6 text-white transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
