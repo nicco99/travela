e68fb48aa55e0ee8a34792788226a01f93f2f5a3
Tressie Muthuri <tressiemuthuri@gmail.com>
2022-12-06 09:48:03 +0300

    TRAV-32 completed dashboard

--- src/App/components/Dashboard/SideBar/index.jsx
+++ src/App/components/Dashboard/SideBar/index.jsx
@@ -2,14 +2,14 @@ import { Link } from "react-router-dom";
 function SideBar() {
   return (
     <aside className="w-80 sticky hidden sm:block" aria-label="Sidebar">
-      <div class="overflow-y-auto py-auto  px-3 bg-black  dark:bg-gray-800 h-screen rounded-md">
+      <div class="overflow-y-auto py-auto  px-3 bg-blue-400  dark:bg-gray-800 h-screen rounded-md">
         <ul class="space-y-2">
           <li>
             <Link className="flex items-center py-4 px-1">
               <img
                 src="https://thumbs.dreamstime.com/b/continuous-one-line-drawing-front-view-bus-stopped-bus-stop-waiting-passengers-to-get-off-then-223157349.jpg"
                 alt="logo"
-                className="w-20 mt-0 rounded-xl"
+                className="w-20 mt-0 rounded-full"
               />
               <span className="font-semibold text-white text-lg">TRAVELA</span>
             </Link>
@@ -59,7 +59,7 @@ function SideBar() {
                 <path d="M3 5a2 2 0 012-2h1a1 1 0 010 2H5v7h2l1 2h4l1-2h2V5h-1a1 1 0 110-2h1a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2V5z"></path>
               </svg>
               <span className="flex-1 ml-3 whitespace-nowrap"> Book a trip</span>
-              <span className="inline-flex justify-center items-center p-5 ml-3 w-3 h-3 text-sm font-medium text-white bg-black rounded-full dark:bg-black dark:text-blue-200">
+              <span className="inline-flex justify-center items-center p-5 ml-3 w-3 h-3 text-sm font-medium text-white bg-cyan-900 rounded-full dark:bg-black dark:text-blue-200">
                 Book
               </span>
             </Link>
@@ -67,24 +67,6 @@ function SideBar() {
           <li>
             <Link
               to="user_profile"
-              className="flex items-center p-2 text-base font-2xl text-white hover:text-blacktext-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-              <svg
-                aria-hidden="true"
-                className="flex-shrink-0 w-6 h-6 text-white hover:text-black transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg">
-                <path
-                  fill-rule="evenodd"
-                  d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-                  clip-rule="evenodd"></path>
-              </svg>
-              <span className="flex-1 ml-3 whitespace-nowrap">User Profile</span>
-            </Link>
-          </li>
-          <li>
-            {/* <Link
-              to="settings"
               className="flex items-center p-2 text-base font-2xl rounded-lg dark:text-white hover:bg-gray-100 text-white hover:text-black">
               <svg
                 aria-hidden="true"
@@ -97,8 +79,8 @@ function SideBar() {
                   d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z"
                   clip-rule="evenodd"></path>
               </svg>
-              <span className="flex-1 ml-3 whitespace-nowrap">Settings</span>
-            </Link> */}
+              <span className="flex-1 ml-3 whitespace-nowrap">User Profile</span>
+            </Link>
           </li>
           <li>
             <Link
@@ -117,24 +99,6 @@ function SideBar() {
               </svg>
               <span className="flex-1 ml-3 whitespace-nowrap">Logout</span>
             </Link>
-          </li>
-          <li>
-            {/* <Link
-              to="delete_acc"
-              className="flex items-center p-2 text-base font-normal  rounded-lg dark:text-white hover:bg-gray-100 text-white hover:text-black">
-              <svg
-                aria-hidden="true"
-                className="flex-shrink-0 w-6 h-6 text-white hover:text-black transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg">
-                <path
-                  fill-rule="evenodd"
-                  d="M5 4a3 3 0 00-3 3v6a3 3 0 003 3h10a3 3 0 003-3V7a3 3 0 00-3-3H5zm-1 9v-1h5v2H5a1 1 0 01-1-1zm7 1h4a1 1 0 001-1v-1h-5v2zm0-4h5V8h-5v2zM9 8H4v2h5V8z"
-                  clip-rule="evenodd"></path>
-              </svg>
-              <span className="flex-1 ml-3 whitespace-nowrap">Delete account</span>
-            </Link> */}
           </li>
         </ul>
         <ul className="pt-4 mt-4 space-y-2 border-t border-gray-200 dark:border-gray-700">
