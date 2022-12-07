549043e96b5dd31b5e2305c8a458bad00beca86d
Tressie Muthuri <tressiemuthuri@gmail.com>
2022-12-07 05:00:13 +0300

    TRAV-27 displayed data

--- src/App/components/Dashboard/Profile/index.jsx
+++ src/App/components/Dashboard/Profile/index.jsx
@@ -5,7 +5,7 @@ import React, { useState, useEffect } from "react";
 // useSelector from the redux store allows you to extract data from the Redux store state, using a selector function
 
 function Profile() {
-  const [passenger, setPassenger] = useState([]);
+  const [passengers, setPassengers] = useState([]);
   // the default userName is set to an empty list and the setUserName used to update the value of the name
 
   // run the uName function when the page loads
@@ -15,10 +15,10 @@ function Profile() {
 
   const details = () => {
     axios
-      .get("http://localhost:3000/passengers")
+      .get("http://localhost:3000/bookings")
       .then((response) => {
         console.log(response);
-        setPassenger(response.data)
+        setPassengers(response.data);
       })
       .catch((error) => {
         console.log(error);
@@ -28,18 +28,25 @@ function Profile() {
   return (
     <div className="w-full flex flex-col grid grid-cols-1">
       <div className="w-28 rounded-full h-28 bg-slate-400 items-center"></div>
-      <div className="flex flex-col w-1/2">
-        <label>Username:</label>
-        <input className="border rounded-md p-2" value="" />
+      <div className="container">
+        {passengers.map((passenger) => (
+          <div className="card" key={passenger.id}>
+        <label className="flex flex-col w-1/2">Email:</label>
+            <input className="border rounded-md p-2" value={passenger.status}/>
+            <label className="flex flex-col w-1/2">phone Number:</label>
+        <input className="border rounded-md p-2" value={passenger.status}/>
+          </div>
+        ))}
       </div>
-      <div className="flex flex-col w-1/2">
+      
+      {/* <div className="flex flex-col w-1/2">
         <label>Email:</label>
         <input className="border rounded-md p-2" />
-      </div>
-      <div className="flex flex-col w-1/2">
+      </div> */}
+      {/* <div className="flex flex-col w-1/2">
         <label>phone Number:</label>
         <input className="border rounded-md p-2" />
-      </div>
+      </div> */}
       <div className="flex flex-row w-1/2 justify-round">
         <button className="bg-blue-400 px-3 rounded-md">Update</button>
         <button className="bg-red-400 px-3 rounded-md">Delete</button>
