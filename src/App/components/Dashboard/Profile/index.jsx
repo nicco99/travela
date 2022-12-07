c1f86f20159d04d1c0a402e51f9d6cc843ae5cd7
Tressie Muthuri <tressiemuthuri@gmail.com>
2022-12-07 04:34:07 +0300

    TRAV-27 fetched data

--- src/App/components/Dashboard/Profile/index.jsx
+++ src/App/components/Dashboard/Profile/index.jsx
@@ -1,16 +1,36 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+
 // Fragments let you group a list of children without adding extra nodes to the DOM
 // useSelector from the redux store allows you to extract data from the Redux store state, using a selector function
 
 function Profile() {
-  const [ userName, setUserName ]= useState([])
+  const [passenger, setPassenger] = useState([]);
   // the default userName is set to an empty list and the setUserName used to update the value of the name
+
+  // run the uName function when the page loads
+  useEffect(() => {
+    details();
+  }, []);
+
+  const details = () => {
+    axios
+      .get("http://localhost:3000/passengers")
+      .then((response) => {
+        console.log(response);
+        setPassenger(response.data)
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div className="w-full flex flex-col grid grid-cols-1">
       <div className="w-28 rounded-full h-28 bg-slate-400 items-center"></div>
       <div className="flex flex-col w-1/2">
         <label>Username:</label>
-        <input className="border rounded-md p-2" value={user.username} />
+        <input className="border rounded-md p-2" value="" />
       </div>
       <div className="flex flex-col w-1/2">
         <label>Email:</label>
